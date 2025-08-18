6c94f2e33503db062d4b4f02bfa50e4652986478
hyunjincoconut <hyunjin@coconutsilo.com>
2025-08-18 18:12:46 +0900

    feat: android 이미지 추가

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "logo180.png",'maskable-icon-512x512.png'],
+      includeAssets: ['pwa-192x192.png', 'pwa-512x512.png','pwa-512x512.png','pwa-512x512.png'],
       manifest: {
         short_name: "KK Delivery",
         name: "KOKKOK Delivery",
@@ -17,22 +17,29 @@ export default defineConfig({
         background_color: "#171717",
         icons: [
           {
-            src: "favicon.ico",
-            sizes: "256x256 180x180 128x128 64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src: "logo180.png",
-            sizes: "180x180",
-            type: "image/png",
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
-            src: 'maskable-icon-512x512.png',
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
           }
-        ],
+        ]
+        
       },
     }),
   ],
