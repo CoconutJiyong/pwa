4c2a28b86a71a503ce08e5b8df26fd23b2535a97
hyunjincoconut <hyunjin@coconutsilo.com>
2025-08-20 14:32:08 +0900

    feat: maskable 삭제

--- vite.config.js
+++ vite.config.js
@@ -40,12 +40,7 @@ export default defineConfig({
             type: 'image/png',
             purpose: 'any'
           },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
+      
         ]
         
       },
