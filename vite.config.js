3d3d2cb1d926d6619245f061dc4ff63f7a177457
hyunjincoconut <hyunjin@coconutsilo.com>
2025-08-20 14:37:23 +0900

    feat: vite.config.ts 원복

--- vite.config.js
+++ vite.config.js
@@ -15,15 +15,7 @@ export default defineConfig({
         display: "standalone",
         theme_color: "#ffffff",
         background_color: "#171717",
-        icons: [  {
-          src: "favicon.ico",
-          sizes: "256x256 180x180 128x128 64x64 32x32 24x24 16x16",
-          type: "image/x-icon",
-        },   {
-          src: 'logo180.png',
-          sizes: '180x180',
-          type: 'image/png'
-        },
+        icons: [
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
@@ -40,7 +32,12 @@ export default defineConfig({
             type: 'image/png',
             purpose: 'any'
           },
-      
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
         ]
         
       },
