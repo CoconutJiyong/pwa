fb4093b1383a760f15d8b06d586527b7e100722d
hyunjincoconut <hyunjin@coconutsilo.com>
2025-08-20 14:30:37 +0900

    feat: favicon 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,11 @@ export default defineConfig({
         display: "standalone",
         theme_color: "#ffffff",
         background_color: "#171717",
-        icons: [    {
+        icons: [  {
+          src: "favicon.ico",
+          sizes: "256x256 180x180 128x128 64x64 32x32 24x24 16x16",
+          type: "image/x-icon",
+        },   {
           src: 'logo180.png',
           sizes: '180x180',
           type: 'image/png'
